95a2e77ef2f53029d87b9d0990200380b90e74a1
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-09-05 22:31:09 -0400

    Learned heading and paragraph tags.

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,6 +2,27 @@ export default function Lab1() {
     return (
         <div id="wd-lab1">
             <h2> Lab 1 </h2>
+            <h3> 2. 6 Heading Tags </h3>
+            <h4>This is h4 heading. </h4>
+            <h5>This is h5 heading. </h5>
+            <h6>This is h6 heading. </h6>
+
+            <h2> 2. 7 Paragraph Tags </h2>
+            <p id={"wd-p-1"}>
+                So this is how to make a paragraph. I usually cannot make Lorem
+                Ipsum-ish passages, but I guess we have to make do of what we
+                have for now.
+            </p>
+
+            <p id={"wd-p-2"}>
+                This is another paragraph. I have run out of what to say for
+                the text, but since it is close to midnight I guess I will spit
+                out gibberish until the number of words satisfy whoever decided
+                to have a minimum.
+            </p>
+
+            <h2> 2. 8 </h2>
+
         </div>
     );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,6 +3,7 @@ import Lab1 from './Lab1'
 export default function Index() {
     return (
         <div id={"wd-labs"}>
+            <b> Hui Juhn Kim </b>
             <h1> Labs </h1>
             <Lab1 />
         </div>
