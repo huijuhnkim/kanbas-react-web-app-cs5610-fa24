91d6755c902f2ff694c1e24fe3d0fb5184245458
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-09-05 20:55:00 -0400

    changed text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO WEB DEV!
+          !!!!WELCOME TO WEB DEV!!!!
         </p>
         <a
           className="App-link"
