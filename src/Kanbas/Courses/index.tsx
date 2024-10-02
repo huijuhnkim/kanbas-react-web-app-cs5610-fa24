d2fdd8ec97add35d3772da19f219a9c1da5b5a49
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-10-02 15:35:32 -0400

    starting kanbas

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -8,26 +8,17 @@ export default function Courses() {
     return (
         <div id="wd-courses">
             <h2>Course 1234</h2>
-            <hr />
-            <table>
-                <tr>
-                    <td valign="top">
-                        <CoursesNavigation />
-                    </td>
-                    <td valign="top">
-                        <Routes>
-                            <Route path="/"
-                                   element={<Navigate to="Home" />} />
-                            <Route path="Home" element={<Home />} />
-                            <Route path="Modules" element={<Modules />} />
-                            <Route path="Assignments"
-                                   element={<Assignments />} />
-                            <Route path="Assignments/:aid"
-                                   element={<AssignmentEditor/>} />
-                            <Route path="People" element={<h3>People</h3>} />
-                        </Routes>
-                    </td>
-                </tr>
-            </table>
+                <CoursesNavigation />
+                <Routes>
+                    <Route path="/"
+                           element={<Navigate to="Home" />} />
+                    <Route path="Home" element={<Home />} />
+                    <Route path="Modules" element={<Modules />} />
+                    <Route path="Assignments"
+                           element={<Assignments />} />
+                    <Route path="Assignments/:aid"
+                           element={<AssignmentEditor/>} />
+                    <Route path="People" element={<h3>People</h3>} />
+                </Routes>
         </div>
     );}
