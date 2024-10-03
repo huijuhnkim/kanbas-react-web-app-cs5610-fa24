f0e03b0c40f43a3c85cd1c196628a14f095ccd20
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-10-03 19:44:19 -0400

    all except styling assignments

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -5,49 +5,49 @@ export default function CoursesNavigation() {
             className={"wd list-group fs-5 rounded-0 "}>
             <Link id="wd-course-home-link"
                   className={"list-group-item active border border-0"}
-                  to="/Kanbas/Courses/1234/Home"
+                  to={"/Kanbas/Courses/1234/Home"}
                     style={{fontSize: 16}}>
                     Home
             </Link>
             <Link id="wd-course-modules-link"
                   className={"list-group-item border text-danger border-0"}
-                  to="/Kanbas/Courses/1234/Modules"
+                  to={"/Kanbas/Courses/1234/Modules"}
                   style={{fontSize: 16}}>
                 Modules
             </Link>
             <Link id="wd-course-piazza-link"
                   className={"list-group-item border text-danger border-0"}
-                  to="/Kanbas/Courses/1234/Piazza"
+                  to={"/Kanbas/Courses/1234/Piazza"}
                   style={{fontSize: 16}}>
                 Piazza
             </Link>
             <Link id="wd-course-zoom-link"
                   className={"list-group-item border text-danger border-0"}
-                  to="/Kanbas/Courses/1234/Zoom"
+                  to={"/Kanbas/Courses/1234/Zoom"}
                   style={{fontSize: 16}}>
                 Zoom
             </Link>
             <Link id="wd-course-quizzes-link"
                   className={"list-group-item border text-danger border-0"}
-                  to="/Kanbas/Courses/1234/Assignments"
+                  to={"/Kanbas/Courses/1234/Assignments"}
                   style={{fontSize: 16}}>
                 Assignments
             </Link>
             <Link id="wd-course-assignments-link"
                   className={"list-group-item border text-danger border-0"}
-                  to="/Kanbas/Courses/1234/Quizzes"
+                  to={"/Kanbas/Courses/1234/Quizzes"}
                   style={{fontSize: 16}}>
                 Quizzes
             </Link>
             <Link id="wd-course-grades-link"
                   className={"list-group-item border text-danger border-0"}
-                  to="/Kanbas/Courses/1234/Grades"
+                  to={"/Kanbas/Courses/1234/Grades"}
                   style={{fontSize: 16}}>
                 Grades
             </Link>
             <Link id="wd-course-people-link"
                   className={"list-group-item border text-danger border-0"}
-                  to="/Kanbas/Courses/1234/People"
+                  to={"/Kanbas/Courses/1234/People"}
                   style={{fontSize: 16}}>
                 People
             </Link>
