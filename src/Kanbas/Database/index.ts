c7fcac1fa64c7d28f75dbecdd041dc25af77f262
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-10-17 22:13:31 -0400

    Started Kanbas implementation

---
+++ src/Kanbas/Database/index.ts
@@ -0,0 +1,5 @@
+import courses from "./courses.json";
+import modules from "./modules.json";
+import enrollments from "./enrollments.json";
+import users from "./users.json";
+export {  courses, modules, enrollments, users  };
