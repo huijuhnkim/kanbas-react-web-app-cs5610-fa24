c3acdb52baaf0fdeb71e7a683f4ad2afba98e6c4
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-10-18 00:36:31 -0400

    Finished Kanbas implementation

--- src/Labs/Lab3/todos/TodoList.tsx
+++ src/Labs/Lab3/todos/TodoList.tsx
@@ -3,7 +3,7 @@ import todos from "./todos.json";
 const TodoList = () => {
     return(
         <>
-            <h3>Todo List</h3>
+            <h3>Todu List</h3>
             <ul className="list-group">
                 { todos.map(todo => {
                     return(<TodoItem todo={todo}/>);
