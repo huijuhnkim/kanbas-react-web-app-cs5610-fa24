c330769e322da46a73a61ee1f2a3acfb232d2e48
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-10-18 00:31:40 -0400

    Finished Kanbas implementation

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -2,6 +2,13 @@ import { courses } from "../Database";
 import { FaAlignJustify } from "react-icons/fa6";
 // import { Navigate, Route, Routes} from "react-router";
 import { useParams, useLocation } from "react-router";
+import Home from "./Home";
+import CoursesNavigation from "./Navigation";
+import {Navigate, Route, Routes} from "react-router-dom";
+import Modules from "./Modules";
+import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/Editor";
+import PeopleTable from "./People/Table";
 
 export default function Courses() {
     const { cid } = useParams();
@@ -11,12 +18,47 @@ export default function Courses() {
     return (
         <div id="wd-courses">
             <h2 className="text-danger">
-                <FaAlignJustify className="me-3 fs-4 mb-1" />
+                <FaAlignJustify className="me-3 fs-4 mb-1"/>
                 {course && course.name} &gt; {pathname.split("/")[4]}
             </h2>
 
+            <div className="d-flex">
+                <div className={"d-none d-xl-block"}>
+                    <CoursesNavigation/>
+                </div>
 
+                <div className={"flex-fill"}>
+                    <Routes>
+                        <Route path="/"
+                               element={<Navigate to="Home"/>}/>
+                        <Route path="Home" element={<Home/>}/>
+                        <Route path="Modules" element={<Modules/>}/>
+                        <Route path="Assignments"
+                               element={<Assignments/>}/>
+                        <Route path="Assignments/:aid"
+                               element={<AssignmentEditor/>}/>
+                        <Route path="People" element={<PeopleTable/>}/>
+                    </Routes>
+                </div>
+            </div>
         </div>
-    );
 
+        // <div id="wd-courses">
+        //     <h2 className="text-danger">
+        //         <FaAlignJustify className="me-4 fs-4 mb-1"/>
+        //         Course 1234 </h2>
+        //     <hr/>
+        //     <div className="d-flex">
+        //         <div className="d-none d-md-block">
+        //             <CoursesNavigation/>
+        //             <Routes>
+        //                 <Route path="Home" element={<Home />} />
+        //                 <Route path="Modules" element={<Modules />} />
+        //                 <Route path="Assignments" element={<Assignments />} />
+        //                 <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+        //             </Routes>
+        //         </div>
+        //     </div>
+        // </div>
+    );
 }
--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -2,4 +2,5 @@ import courses from "./courses.json";
 import modules from "./modules.json";
 import enrollments from "./enrollments.json";
 import users from "./users.json";
-export {  courses, modules, enrollments, users  };
+import assignments from "./assignments.json";
+export {  courses, modules, enrollments, users, assignments  };
