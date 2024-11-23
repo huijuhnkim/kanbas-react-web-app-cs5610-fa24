4f30387e73bb00315f7a824762c816906cc422d1
huijuhnkim <huijuhnkim.dev@gmail.com>
2024-11-22 21:16:46 -0500

    implemented profile section

--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -1,19 +1,29 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import * as client from "./client";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "./reducer";
 export default function Signup() {
+    const [credentials, setCredentials] = useState<any>({});
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const signup = async () => {
+        const user = await client.signup(credentials);
+        dispatch(setCurrentUser(user));
+        navigate("/Kanbas/Account/Profile");
+    };
     return (
-        <div id={"wd-signup-screen"}>
-            <h3> Sign up</h3>
-            <input placeholder={"username"}
-                   className={"form-control mb-2"} />
-            <input placeholder={"password"}
-                   className={"form-control mb-2"}
-                   type={"password"}/>
-
-            <Link to={"/Kanbas/Account/Profile"}
-                  className={"btn btn-primary w-100"}>
-                Sign up </Link> <br/>
-            <Link to={"/Kanbas/Account/Signin"}> Sign in </Link>
+        <div className="wd-signup-screen">
+            <h1>Sign up</h1>
+            <input defaultValue={credentials.username}
+                   onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+                   className="form-control mb-2" placeholder="username" id="wd-username" />
+            <input defaultValue={credentials.password}
+                   onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+                   className="form-control mb-2" placeholder="password" type="password" id="wd-password" />
+            <button onClick={signup} className="wd-signup-btn btn btn-primary mb-2 w-100"> Sign up</button>
+            <br/>
+            <Link to="/Kanbas/Account/Signin" className="wd-signin-link">Sign in</Link>
         </div>
-    )
+    );
 }
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,10 +3,11 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
-import * as db from "./Database";
 import { useState } from "react";
 import "./styles.css"
 import ProtectedRoute from "./Account/ProtectedRoute";
+import * as db from "./Database";
+import Session from "./Account/Session";
 
 export default function Kanbas() {
     const [courses, setCourses] = useState<any[]>(db.courses);
@@ -33,32 +34,34 @@ export default function Kanbas() {
     };
 
     return (
-        <div id="wd-kanbas">
-            <KanbasNavigation/>
-            <div className={"wd-main-content-offset p-3"}>
-                <Routes>
-                    <Route path="/" element={<Navigate to="Dashboard"/>}/>
-                    <Route path="/Account/*" element={<Account/>}/>
-                    <Route path={"/Dashboard/*"} element={
-                        <ProtectedRoute>
-                            <Dashboard
-                            courses={courses}
-                            course={course}
-                            setCourse={setCourse}
-                            addNewCourse={addNewCourse}
-                            deleteCourse={deleteCourse}
-                            updateCourse={updateCourse}/>
-                        </ProtectedRoute>
-                    }/>
-                    <Route path="/Courses/:cid/*" element={
-                        <ProtectedRoute>
-                            <Courses courses={courses}/>
-                        </ProtectedRoute>}/>
-                    <Route path="/Calendar" element={<h1>Calendar</h1>} />
-                    <Route path="/Inbox" element={<h1>Inbox</h1>} />
-                </Routes>
+        <Session>
+            <div id="wd-kanbas">
+                <KanbasNavigation/>
+                <div className={"wd-main-content-offset p-3"}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Dashboard"/>}/>
+                        <Route path="/Account/*" element={<Account/>}/>
+                        <Route path={"/Dashboard/*"} element={
+                            <ProtectedRoute>
+                                <Dashboard
+                                courses={courses}
+                                course={course}
+                                setCourse={setCourse}
+                                addNewCourse={addNewCourse}
+                                deleteCourse={deleteCourse}
+                                updateCourse={updateCourse}/>
+                            </ProtectedRoute>
+                        }/>
+                        <Route path="/Courses/:cid/*" element={
+                            <ProtectedRoute>
+                                <Courses courses={courses}/>
+                            </ProtectedRoute>}/>
+                        <Route path="/Calendar" element={<h1>Calendar</h1>} />
+                        <Route path="/Inbox" element={<h1>Inbox</h1>} />
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </Session>
     );
 }
 
